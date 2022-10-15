2de0b0983dce7769a906945fca58553be5e7b812
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-10-15 16:00:10 +0100

    Full CRUD functionality - NO STYLING

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: 'tasks#new'
-  get 'tasks', to: 'tasks#index'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+
+  resources :tasks
 end
